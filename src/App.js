7147863ea7cf972fa3e2fe06cf4540b11f560230
vale75020 <valentinosuma@hotmail.com>
2019-06-07 11:28:18 +0200

    droppable and draggable components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "./App.css";
-import "@atlaskit/css-reset"
+import "@atlaskit/css-reset";
 import initialData from "./initial-data";
 import Column from "./components/Column";
-
+import { DragDropContext } from "react-beautiful-dnd";
 
 class App extends React.Component {
   state = initialData;
 
+  onDragEnd = result => { // update state
+    //TO DO render our column
+  }
   render() {
-    return this.state.columnOrder.map((columnId) => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-      return <Column key={column.id} column={column} tasks={tasks} />
-    });
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
   }
 }
 
