b6e957f5dedddbdc9170eee3dd9ac75fb915542f
vale75020 <valentinosuma@hotmail.com>
2019-06-19 10:33:42 +0200

    Updates

---
+++ precache-manifest.08fd0f309175fdfd64437a983fdad979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4e1c95871dd41bfec3529c1a5ceb64",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "a569cbc56a69eaafb317",
+    "url": "/task-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "689f54362a25425ffaf2",
+    "url": "/task-app/static/js/2.4f1c6138.chunk.js"
+  },
+  {
+    "revision": "a569cbc56a69eaafb317",
+    "url": "/task-app/static/js/main.3302181a.chunk.js"
+  },
+  {
+    "revision": "d06c256c44913070d1fb",
+    "url": "/task-app/static/js/runtime~main.03238dc8.js"
+  }
+]);
